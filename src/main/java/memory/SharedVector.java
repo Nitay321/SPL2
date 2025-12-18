b42938824b5da0a192235ea6cab30313515d06cf
PizzaBoii <sassongal1@gmail.com>
2025-12-18 15:54:45 +0200

    t

--- src/main/java/memory/SharedVector.java
+++ src/main/java/memory/SharedVector.java
@@ -9,49 +9,101 @@ public class SharedVector {
     private ReadWriteLock lock = new java.util.concurrent.locks.ReentrantReadWriteLock();
 
     public SharedVector(double[] vector, VectorOrientation orientation) {
-        // TODO: store vector data and its orientation
+        this.vector = vector;
+        this.orientation = orientation;
     }
 
     public double get(int index) {
         // TODO: return element at index (read-locked)
-        return 0;
+        readLock();
+        try{
+        return vector[index];}
+        finally {
+            readUnlock();
+        }
     }
 
     public int length() {
-        // TODO: return vector length
-        return 0;
+        
+        // TODO: return vector lengt
+        readLock();
+        try {
+            return vector.length;
+        } finally {
+            readUnlock();
+        }
     }
 
     public VectorOrientation getOrientation() {
         // TODO: return vector orientation
-        return null;
-    }
+    readLock();
+    try {
+        return orientation;
+    } finally {
+        readUnlock();}    }
 
     public void writeLock() {
+        lock.writeLock().lock();
         // TODO: acquire write lock
     }
 
     public void writeUnlock() {
+        lock.writeLock().unlock();
         // TODO: release write lock
     }
 
     public void readLock() {
+        lock.readLock().lock();
         // TODO: acquire read lock
     }
 
     public void readUnlock() {
+        lock.readLock().unlock();
         // TODO: release read lock
     }
 
     public void transpose() {
+        writeLock();
+        try { 
+            if (orientation == VectorOrientation.ROW_MAJOR) {
+                orientation = VectorOrientation.COLUMN_MAJOR;
+            } else {
+                orientation = VectorOrientation.ROW_MAJOR;
+            }
+        } finally {
+            writeUnlock();
+        }
+        
+        
+        
         // TODO: transpose vector
     }
 
     public void add(SharedVector other) {
+        writeLock();
+        other.readLock();
+        try { 
+            for (int i = 0; i < vector.length; i++) {
+                vector[i] += other.vector[i];
+            }
+        } finally {
+            writeUnlock();
+            other.readUnlock();
+        }
+        
         // TODO: add two vectors
     }
 
     public void negate() {
+        writeLock();
+        try { 
+            for (int i = 0; i < vector.length; i++) {
+                vector[i] = -vector[i];
+            }
+        } finally {
+            writeUnlock();
+        }
+
         // TODO: negate vector
     }
 
